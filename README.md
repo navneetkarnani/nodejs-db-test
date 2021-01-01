1b0b4bf739a0290a0171f5c3d68be8fec137cade
Navneet Karnani <navneet@mocsh.com>
2021-01-01 08:47:25 +0000

    add nodejs code with connection to database

---
+++ README.md
@@ -0,0 +1,4 @@
+Supporting repository for the article at:
+https://blog.mandraketech.in//dev-environment-using-docker-compose-and-vs-code
+
+This application uses the environment variable MONGO_DB to connect to the database.
---
+++ app.js
@@ -0,0 +1,49 @@
+if (!process.env.MONGO_DB) {
+  throw new Error('env MONGO_DB not defined. Cannot proceed.')
+}
+
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var dbInit = require('./db/db-init');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+dbInit(process.env.MONGO_DB);
+
+module.exports = app;
